80252c880d38bff96031a29418b6b84be5509657
sonukumar82082-gif <sonukumar82082@gmail.com>
2026-09-15 10:11:29 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [{ source: '/', destination: '/index.html' }],
+    }
+  },
+}
+
+export default nextConfig
